e90dcc95bf32e4af25510be229d30ced5f58207b
Swdnmd <157853959+Swdnmd@users.noreply.github.com>
2024-01-26 15:42:18 -0800

    Update select.py

--- pyensemble/experiments/debug/select.py
+++ pyensemble/experiments/debug/select.py
@@ -6,14 +6,14 @@ from pyensemble.models import Stimulus
 import os, random
 import pdb
 
-def select_audio(request,*args,**kwargs):
-    name = kwargs.get('name',None)
-    playlist = kwargs.get('playlist',None)
+
+def select_audio(request, *args, **kwargs):
+    name = kwargs.get('name', None)
+    playlist = kwargs.get('playlist', None)
     use_jspsych = kwargs.get('use_jspsych', True)
 
     stimulus = None
     timeline = []
-
     # Fetch the stimulus object if we are giving a name
     if name:
         stimulus = Stimulus.objects.get(name=name)
@@ -24,9 +24,8 @@ def select_audio(request,*args,**kwargs):
 
         # Select one at random
         if stimuli.count():
-            stimidx = random.randrange(0,stimuli.count())
+            stimidx = random.randrange(0, stimuli.count())
             stimulus = stimuli[stimidx]
-
 
     if stimulus and use_jspsych:
         if settings.USE_AWS_STORAGE:
@@ -50,108 +49,106 @@ def select_audio(request,*args,**kwargs):
             'type': 'jsPsychHtmlKeyboardResponse',
             'stimulus': "Welcome to the experiment. Press any key to begin.",
         }
-        
-        trial_instructions = {
+
+        #       trial_instructions = {
+        #           'type': 'jsPsychHtmlKeyboardResponse',
+        #           'stimulus': `
+        #   <p>In this experiment, a pair of letters will appear in the center
+        #   of the screen, one after the other.</p>
+        #   <p>If you see the sequence <strong>A-X</strong>,
+        #   press the letter J on the keyboard as fast as you can.</p>
+        #   <p>For any other letter sequence, press the letter F.</p>
+        #   <p>Press any key to begin.</p>
+        # `,
+        #           post_trial_gap: 2000
+        #       }
+
+        trial_fixation = {
             'type': 'jsPsychHtmlKeyboardResponse',
-            'stimulus': `
-    <p>In this experiment, a pair of letters will appear in the center 
-    of the screen, one after the other.</p>
-    <p>If you see the sequence <strong>A-X</strong>, 
-    press the letter J on the keyboard as fast as you can.</p>
-    <p>For any other letter sequence, press the letter F.</p>
-    <p>Press any key to begin.</p>
-  `,
-            post_trial_gap: 2000
+            'stimulus': '<div style="font-size:60px;">+</div>',
+            'choices': 'NO_KEYS',
+            'trial_duration': 250,
+            'post_trial_gap': 750,
         }
-        trial = {
-            'type': 'audio-keyboard-response',
-            'stimulus': stimulus_url,
-            'choices': 'none',
-            'click_to_start': True,
-            'trial_ends_after_audio': True,
-            'trial_duration': 3000,
+
+        trial_cue = {
+            'type': 'jsPsychHtmlKeyboardResponse',
+            'stimulus': jsPsych.timelineVariable('cue_stimulus'),
+            'choices': 'NO_KEYS',
+            'trial_duration': 250,
+            'post_trial_gap': 750,
         }
-        trial = {
-            'type': 'audio-keyboard-response',
-            'stimulus': stimulus_url,
-            'choices': 'none',
-            'click_to_start': True,
-            'trial_ends_after_audio': True,
-            'trial_duration': 3000,
+
+        trial_probe = {
+            'type': 'jsPsychHtmlKeyboardResponse',
+            'stimulus': jsPsych.timelineVariable('probe_stimulus'),
+            'choices': 'NO_KEYS',
+            'trial_duration': 250,
+            'post_trial_gap': 750,
         }
-        trial = {
-            'type': 'audio-keyboard-response',
-            'stimulus': stimulus_url,
-            'choices': 'none',
-            'click_to_start': True,
-            'trial_ends_after_audio': True,
-            'trial_duration': 3000,
+
+        trial_response = {
+            'type': 'jsPsychHtmlKeyboardResponse',
+            'stimulus': '<div style="font-size:60px;">+</div>',
+            'choices': ['f', 'j'],
         }
-        trial = {
-            'type': 'audio-keyboard-response',
-            'stimulus': stimulus_url,
-            'choices': 'none',
-            'click_to_start': True,
-            'trial_ends_after_audio': True,
-            'trial_duration': 3000,
+
+        trial_slow_response_feedback = {
+            'type': 'jsPsychHtmlKeyboardResponse',
+            'stimulus': '',
+            'choices': 'NO_KEYS',
         }
-        trial = {
-            'type': 'audio-keyboard-response',
-            'stimulus': stimulus_url,
-            'choices': 'none',
-            'click_to_start': True,
-            'trial_ends_after_audio': True,
-            'trial_duration': 3000,
+
+        trial_short_break = {
+            'type': 'jsPsychVideoKeyboardResponse',
+            'stimulus': ['vid\Video effort attribution 1080p.mp4'],
+            'prompt': '<p>Take a break! Press any key when you are ready to continue.</p>',
+            'width': 800,
+            'height': 450,
+            'autoplay': true,
+            'controls': false,
+            'trial_ends_after_video': false,
+            'choices': 'ALL_KEYS'
         }
-        trial = {
-            'type': 'audio-keyboard-response',
-            'stimulus': stimulus_url,
-            'choices': 'none',
-            'click_to_start': True,
-            'trial_ends_after_audio': True,
-            'trial_duration': 3000,
+
+        trial_long_break = {
+            'type': 'jsPsychHtmlKeyboardResponse',
+            'stimulus': '<p>You may take a 2-minute break. Press any key when you are ready to continue.</p>',
+            'trial_duration': 120000,
         }
-        trial = {
-            'type': 'audio-keyboard-response',
-            'stimulus': stimulus_url,
-            'choices': 'none',
-            'click_to_start': True,
-            'trial_ends_after_audio': True,
-            'trial_duration': 3000,
-        }
-        trial = {
-            'type': 'audio-keyboard-response',
-            'stimulus': stimulus_url,
-            'choices': 'none',
-            'click_to_start': True,
-            'trial_ends_after_audio': True,
-            'trial_duration': 3000,
-        }
-        trial = {
-            'type': 'audio-keyboard-response',
-            'stimulus': stimulus_url,
-            'choices': 'none',
-            'click_to_start': True,
-            'trial_ends_after_audio': True,
-            'trial_duration': 3000,
-        }
-        trial = {
-            'type': 'audio-keyboard-response',
-            'stimulus': stimulus_url,
-            'choices': 'none',
-            'click_to_start': True,
-            'trial_ends_after_audio': True,
-            'trial_duration': 3000,
-        }
-        trial = {
-            'type': 'audio-keyboard-response',
-            'stimulus': stimulus_url,
-            'choices': 'none',
-            'click_to_start': True,
-            'trial_ends_after_audio': True,
-            'trial_duration': 3000,
-        }
+
+        # trial_test_procedure = {
+        #     'type': 'with-replacement',
+        #     'size': 1,
+        #     'weights': [70, 10, 10, 10],
+        # }
+
+      #   trial_debrief_block = {
+      #       'type': 'jsPsychHtmlKeyboardResponse',
+      #       'stimulus': function() {
+      #
+      #           var trials = jsPsych.data.get().filter({
+      #               task: 'response'
+      #           }); // filtering data where task is 'response'
+      #           var correct_trials = trials.filter({
+      #               correct: true
+      #           });
+      #           var accuracy = Math.round(correct_trials.count() / trials.count() * 100);
+      #           var rt = Math.round(correct_trials.select('rt').mean());
+      #
+      #           return `<p>You responded correctly on ${accuracy}% of the trials.</p>
+      # <p>Your average response time was ${rt}ms.</p>
+      # <p>Press any key to complete the experiment. Thank you!</p>`;
+      #   }
+
         timeline.append(trial)
+        timeline.append(trial_fixation)
+        timeline.append(trial_cue)
+        timeline.append(trial_fixation)
+        timeline.append(trial_probe,)
+        timeline.append(trial_response)
+        timeline.append(trial_slow_response_feedback)
+
 
     stimulus_id = None
     if stimulus:
